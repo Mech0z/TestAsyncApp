9f7fce39d80d4dd1e9575d50b3ea946efaaa405f
Mads Skipper <madsskipper@gmail.com>
2017-03-06 17:48:40 +0100

    Add project files.

---
+++ TestAsyncApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestAsyncApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var result = FirstBlock().Result;
+        }
+
+        public static async Task<bool> FirstBlock()
+        {
+            try
+            {
+                bool result = false;
+                var result1 = SecondBlock();
+                var result2 = SecondBlock();
+
+                await Task.WhenAll(result1, result2);
+                return result1.Result || result2.Result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw e.GetBaseException();
+            }
+            
+        }
+
+        public static async Task<bool> SecondBlock()
+        {
+            return await ThirdBlock();
+        }
+
+        public static async Task<bool> ThirdBlock()
+        {
+            await Task.Delay(1000);
+            throw new Exception("some exception");
+        }
+    }
+}
